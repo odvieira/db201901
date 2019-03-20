6d3c85afde722d489627be23e8f7ab0379f111cf
Pedro Francescon Cittolin <pfcittolin@gmail.com>
2019-03-20 11:57:38 -0300

    Criando todos os arquivos solicitados (herois, herois_good e herois_bad)

--- 01 - Modelos de dados e XML/ReadXMLFile.py
+++ 01 - Modelos de dados e XML/ReadXMLFile.py
@@ -4,6 +4,16 @@ from xml.dom.minidom import parse
 import xml.dom.minidom
 import pandas as pd
 
+atributos = ["id","name","popularity","alignment","gender","height_m","weight_kg","hometown","intelligence","strength","speed","durability","energy_Projection","fighting_Skills"]
+
+def new_csv_line(index,dataFrame,hero):
+	dataFrame.at[index,"id"] = hero.getAttribute("id")
+	for atributo in atributos:
+		if not (atributo=="id"):
+			dataFrame.at[index,atributo] = hero.getElementsByTagName(atributo)[0].firstChild.nodeValue
+
+	return dataFrame
+
 # Open XML document using minidom parser
 DOMTree = xml.dom.minidom.parse("marvel_simplificado.xml")
 
@@ -11,28 +21,27 @@ universe = DOMTree.documentElement
 if universe.hasAttribute("name"):
    print("Root element : %s" % universe.getAttribute("name"))
 
-atributos = ["name","popularity","alignment","gender","height_m","weight_kg","hometown","intelligence","strength","speed","durability","energy_Projection","fighting_Skills"]
 heroes_csv = pd.DataFrame(columns=atributos)
 herois_good = pd.DataFrame(columns=atributos)
 herois_bad = pd.DataFrame(columns=atributos)
+
 # Get all the heroes in the universe
 heroes = universe.getElementsByTagName("hero")
 counter = 0
+counter_good = 0
+counter_bad = 0
+
 for hero in heroes:
-    if(hero.getElementsByTagName("alignment")[0].firstChild.nodeValue == )
-    for atributo in atributos:
-        # print(hero.getElementsByTagName(atributo)[0].firstChild.nodeValue)
-        if(atributo == "alignment"):
+	if(hero.getElementsByTagName("alignment")[0].firstChild.nodeValue == "Good"):
+		new_csv_line(counter_good,herois_good,hero)
+		counter_good += 1
+	elif(hero.getElementsByTagName("alignment")[0].firstChild.nodeValue == "Bad"):
+		new_csv_line(counter_bad,herois_bad,hero)
+		counter_bad +=1
+	
+	new_csv_line(counter,heroes_csv,hero)
+	counter+=1
 
-        heroes_csv.at[counter,atributo] = hero.getElementsByTagName(atributo)[0].firstChild.nodeValue
-    counter+=1
-heroes_csv.to_csv("dadosMarvel/herois.csv",sep=",",index_label="id")
-
-# Print detail of each hero.
-for hero in heroes:
-   print("*****Hero*****")
-   if hero.hasAttribute("id"):
-      print("Id: %s" % hero.getAttribute("id"))
-
-   name = hero.getElementsByTagName('name')[0]
-   print("Name: %s" % name.childNodes[0].data)
+heroes_csv.to_csv("dadosMarvel/herois.csv", index=False)
+herois_good.to_csv("dadosMarvel/herois_good.csv", index=False)
+herois_bad.to_csv("dadosMarvel/herois_bad.csv", index=False)
