ee27d88b3c5872a0aee75df3ef0dfa48ceae3d67
odvieira <d-vieira@live.com>
2019-04-15 21:52:49 -0300

    correção de erros

--- 04 - XML Parsing e inserção de dados/XML_Parsing.py
+++ 04 - XML Parsing e inserção de dados/XML_Parsing.py
@@ -1,164 +1,44 @@
 #!/usr/bin/python3
 
-from xml.dom.minidom import parse
-from urllib.request import urlopen
-from pandas import DataFrame
 from psycopg2 import extras, connect
-
-class Person_DataFrame(DataFrame):
-    def __init__(self):
-        super(Person_DataFrame, self).__init__(
-            columns=["uri", "name", "hometown", "birthdate"])
-
-        self.counter = 0
-
-        return
-
-    def init_from_URL(self, url):
-        domTree = parse(urlopen(url))
-
-        persons = domTree.documentElement.getElementsByTagName("Person")
-
-        for person in persons:
-            self.new_Person_DataFrame_line(person)
-
-        return
-
-    def new_Person_DataFrame_line(self, person):
-
-        for atributo in self.columns:
-            self.at[self.counter, atributo] = person.getAttribute(atributo)
-
-        self.counter += 1
-
-        return
-
-
-class AllLikesMusic_DataFrame(DataFrame):
-    def __init__(self):
-        super(AllLikesMusic_DataFrame, self).__init__(
-            columns=["person", "rating", "bandUri"])
-
-        self.counter = 0
-
-        return
-
-    def init_from_URL(self, url):
-        domTree = parse(urlopen(url))
-
-        allLikesMusic = domTree.documentElement.getElementsByTagName(
-            "LikesMusic")
-
-        for likesMusic in allLikesMusic:
-            self.new_AllLikesMusic_DataFrame_line(likesMusic)
-
-        return
-
-    def new_AllLikesMusic_DataFrame_line(self, likesMusic):
-
-        for atributo in self.columns:
-            self.at[self.counter, atributo] = likesMusic.getAttribute(atributo)
-
-        self.counter += 1
-
-        return
-
-
-class AllLikesMovie_DataFrame(DataFrame):
-    def __init__(self):
-        super(AllLikesMovie_DataFrame, self).__init__(
-            columns=["person", "rating", "movieUri"])
-
-        self.counter = 0
-
-        return
-
-    def init_from_URL(self, url):
-        domTree = parse(urlopen(url))
-
-        allLikesMovie = domTree.documentElement.getElementsByTagName(
-            "LikesMovie")
-
-        for likesMovie in allLikesMovie:
-            self.new_AllLikesMovie_DataFrame_line(likesMovie)
-
-        return
-
-    def new_AllLikesMovie_DataFrame_line(self, likesMovie):
-
-        for atributo in self.columns:
-            self.at[self.counter, atributo] = likesMovie.getAttribute(atributo)
-
-        self.counter += 1
-
-        return
-
-
-class AllKnows_DataFrame(DataFrame):
-    def __init__(self):
-        super(AllKnows_DataFrame, self).__init__(
-            columns=["person", "colleague"])
-
-        self.counter = 0
-
-        return
-
-    def init_from_URL(self, url):
-        domTree = parse(urlopen(url))
-
-        allKnows = domTree.documentElement.getElementsByTagName("Knows")
-
-        for knows in allKnows:
-            self.new_AllKnows_DataFrame_line(knows)
-
-        return
-
-    def new_AllKnows_DataFrame_line(self, allKnows):
-
-        for atributo in self.columns:
-            self.at[self.counter, atributo] = allKnows.getAttribute(atributo)
-
-        self.counter += 1
-
-        return
-
+from parselib import Person_DataFrame, AllKnows_DataFrame, AllLikesMovie_DataFrame, AllLikesMusic_DataFrame
 
 if __name__ == "__main__":
     credentials = "dbname='1901EquipePGDR' user='1901EquipePGDR' host='200.134.10.32' password='793953'"
 
-    # try: # Persons parse
-    #     persons = Person_DataFrame()
-    #     persons.init_from_URL(
-    #         'http://dainf.ct.utfpr.edu.br/~gomesjr/BD1/data/person.xml')
+    try: # Persons parse
+        persons = Person_DataFrame()
+        persons.init_from_URL(
+            'http://dainf.ct.utfpr.edu.br/~gomesjr/BD1/data/person.xml')
 
-    #     values = []
-    #     login = ''
-    #     name = ''
-    #     hometown = ''
+        values = []
+        login = ''
+        name = ''
+        hometown = ''
 
-    #     for i in range(persons.counter):
-    #         for attr in persons.columns:
-    #             if attr == 'uri':
-    #                 login = persons.at[i, attr].split(
-    #                     'http://utfpr.edu.br/CSB30/2019/1/')
-    #             if attr == 'name':
-    #                 name = persons.at[i, attr]
-    #             if attr == 'hometown':
-    #                 hometown = persons.at[i, attr]
+        for i in range(persons.counter):
+            for attr in persons.columns:
+                if attr == 'uri':
+                    login = persons.at[i, attr].split(
+                        'http://utfpr.edu.br/CSB30/2019/1/')
+                if attr == 'name':
+                    name = persons.at[i, attr]
+                if attr == 'hometown':
+                    hometown = persons.at[i, attr]
 
-    #         values.append(tuple([login[1], name, hometown]))
+            values.append(tuple([login[1], name, hometown]))
 
-    #     query = 'INSERT INTO Usuario (Login, Nome, Cidade_natal) VALUES (%s, %s, %s)'
+        query = 'INSERT INTO Usuario (Login, Nome, Cidade_natal) VALUES (%s, %s, %s)'
 
-    #     with connect(credentials) as connection:
-    #         with connection.cursor(cursor_factory=extras.DictCursor) as cur:
-    #             try:
-    #                 # print(cur.mogrify(query, values[0]))
-    #                 cur.executemany(query, values)
-    #             except Exception as e:
-    #                 print(e)
-    # except Exception as e:
-    #     print(e)
+        with connect(credentials) as connection:
+            with connection.cursor(cursor_factory=extras.DictCursor) as cur:
+                try:
+                    # print(cur.mogrify(query, values[0]))
+                    cur.executemany(query, values)
+                except Exception as e:
+                    print(e)
+    except Exception as e:
+        print(e)
 
     try:  # All likes Music
         allLikesMusic = AllLikesMusic_DataFrame()
@@ -177,12 +57,25 @@ if __name__ == "__main__":
 
             values.append(tuple([login[1], int(rating[0]), bandaUri[1]]))
 
-        query = query = 'INSERT INTO UsuarioAvaliaArtistasMusicais (Login, Nota, Id) VALUES (%s, %s, %s)'
+        with connect(credentials) as connection:
+            with connection.cursor(cursor_factory=extras.DictCursor) as cur:
+                try:  # adding artistasmusicais Ids
+                    lst = []
+                    for v in values:
+                        item = tuple([v[2]])
+                        if item not in lst:
+                            lst.append(item)
+
+                    cur.executemany(
+                        'INSERT INTO artistasmusicais (Id) VALUES (%s)', lst)
+                except Exception as e:
+                    print(e)
+
+        query = 'INSERT INTO UsuarioAvaliaArtistasMusicais (Login, Nota, Id) VALUES (%s, %s, %s)'
 
         with connect(credentials) as connection:
             with connection.cursor(cursor_factory=extras.DictCursor) as cur:
                 try:
-                    
                     cur.executemany(query, values)
                 except Exception as e:
                     print(e)
@@ -207,7 +100,21 @@ if __name__ == "__main__":
 
             values.append(tuple([login[1], int(rating[0]), movieUri[1]]))
 
-        query = query = 'INSERT INTO UsuarioAvaliaFilme (Login, Nota, Id) VALUES (%s, %s, %s)'
+        with connect(credentials) as connection:
+            with connection.cursor(cursor_factory=extras.DictCursor) as cur:
+                try:  # adding artistasmusicais Ids
+                    lst = []
+                    for v in values:
+                        item = tuple([v[2]])
+                        if item not in lst:
+                            lst.append(item)
+
+                    cur.executemany(
+                        'INSERT INTO filme (Id) VALUES (%s)', lst)
+                except Exception as e:
+                    print(e)
+
+        query = 'INSERT INTO UsuarioAvaliaFilme (Login, Nota, Id) VALUES (%s, %s, %s)'
 
         with connect(credentials) as connection:
             with connection.cursor(cursor_factory=extras.DictCursor) as cur:
@@ -232,9 +139,13 @@ if __name__ == "__main__":
             login = v[0].split('http://utfpr.edu.br/CSB30/2019/1/')
             colleague = v[1].split('http://utfpr.edu.br/CSB30/2019/1/')
 
-            values.append(tuple([login[1], colleague[1]]))
+            try:
+                values.append(tuple([login[1], colleague[1]]))
+            except Exception as e:
+                print(e)
+                continue
 
-        query = query = 'INSERT INTO UsuarioRegistraConhecidoUsuario (Login, Login_registrado) VALUES (%s, %s)'
+        query = 'INSERT INTO UsuarioRegistraConhecidoUsuario (Login, Login_registrado) VALUES (%s, %s)'
 
         with connect(credentials) as connection:
             with connection.cursor(cursor_factory=extras.DictCursor) as cur:
---
+++ 04 - XML Parsing e inserção de dados/parselib.py
@@ -0,0 +1,120 @@
+from xml.dom.minidom import parse
+from urllib.request import urlopen
+from pandas import DataFrame
+
+class Person_DataFrame(DataFrame):
+    def __init__(self):
+        super(Person_DataFrame, self).__init__(
+            columns=["uri", "name", "hometown", "birthdate"])
+
+        self.counter = 0
+
+        return
+
+    def init_from_URL(self, url):
+        domTree = parse(urlopen(url))
+
+        persons = domTree.documentElement.getElementsByTagName("Person")
+
+        for person in persons:
+            self.new_Person_DataFrame_line(person)
+
+        return
+
+    def new_Person_DataFrame_line(self, person):
+
+        for atributo in self.columns:
+            self.at[self.counter, atributo] = person.getAttribute(atributo)
+
+        self.counter += 1
+
+        return
+
+
+class AllLikesMusic_DataFrame(DataFrame):
+    def __init__(self):
+        super(AllLikesMusic_DataFrame, self).__init__(
+            columns=["person", "rating", "bandUri"])
+
+        self.counter = 0
+
+        return
+
+    def init_from_URL(self, url):
+        domTree = parse(urlopen(url))
+
+        allLikesMusic = domTree.documentElement.getElementsByTagName(
+            "LikesMusic")
+
+        for likesMusic in allLikesMusic:
+            self.new_AllLikesMusic_DataFrame_line(likesMusic)
+
+        return
+
+    def new_AllLikesMusic_DataFrame_line(self, likesMusic):
+
+        for atributo in self.columns:
+            self.at[self.counter, atributo] = likesMusic.getAttribute(atributo)
+
+        self.counter += 1
+
+        return
+
+
+class AllLikesMovie_DataFrame(DataFrame):
+    def __init__(self):
+        super(AllLikesMovie_DataFrame, self).__init__(
+            columns=["person", "rating", "movieUri"])
+
+        self.counter = 0
+
+        return
+
+    def init_from_URL(self, url):
+        domTree = parse(urlopen(url))
+
+        allLikesMovie = domTree.documentElement.getElementsByTagName(
+            "LikesMovie")
+
+        for likesMovie in allLikesMovie:
+            self.new_AllLikesMovie_DataFrame_line(likesMovie)
+
+        return
+
+    def new_AllLikesMovie_DataFrame_line(self, likesMovie):
+
+        for atributo in self.columns:
+            self.at[self.counter, atributo] = likesMovie.getAttribute(atributo)
+
+        self.counter += 1
+
+        return
+
+
+class AllKnows_DataFrame(DataFrame):
+    def __init__(self):
+        super(AllKnows_DataFrame, self).__init__(
+            columns=["person", "colleague"])
+
+        self.counter = 0
+
+        return
+
+    def init_from_URL(self, url):
+        domTree = parse(urlopen(url))
+
+        allKnows = domTree.documentElement.getElementsByTagName("Knows")
+
+        for knows in allKnows:
+            self.new_AllKnows_DataFrame_line(knows)
+
+        return
+
+    def new_AllKnows_DataFrame_line(self, allKnows):
+
+        for atributo in self.columns:
+            self.at[self.counter, atributo] = allKnows.getAttribute(atributo)
+
+        self.counter += 1
+
+        return
